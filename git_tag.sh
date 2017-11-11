9f053933f50ff22965339cf2e6bfb1a60f483515
Valentin Fries <contact@fries.io>
2017-11-11 12:23:29 +0100

    git_tag.sh

---
+++ git_tag.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git config --global user.email "builds@travis-ci.com"
+git config --global user.name "Travis CI"
+GIT_TAG=$TRAVIS_BRANCH-$TRAVIS_BUILD_NUMBER
+git tag $GIT_TAG -a -m "Generated tag from TravisCI"
+git push -q https://$GITHUB_TOKEN@github.com/tomorrow-paper/paper --tags
